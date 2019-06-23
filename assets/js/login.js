fb3fbf1514c6ff3e462c4252f9d89cecf0050815
이재익 <vashstampid03@gmail.com>
2019-06-23 22:37:08 +0900

    Login Flow part Two Complete

---
+++ assets/js/login.js
@@ -0,0 +1,32 @@
+const body = document.querySelector("body");
+const loginForm = document.getElementById("jsLogin");
+const LOGGED_OUT = "loggedOut";
+const LOGGED_IN = "loggedIn";
+const NICKNAME = "nickname";
+const nickname = localStorage.getItem(NICKNAME);
+
+const logIn = nickname => {
+  const socket = io("/");
+  socket.emit("setNickname", { nickname });
+};
+
+if (nickname === null) {
+  body.className = LOGGED_OUT;
+} else {
+  body.className = LOGGED_IN;
+  logIn(nickname);
+}
+
+const handleFormSubmit = e => {
+  e.preventDefault();
+  const input = loginForm.querySelector("input");
+  const { value } = input;
+  input.value = "";
+  localStorage.setItem(NICKNAME, value);
+  body.className = LOGGED_IN;
+  logIn(value);
+};
+
+if (loginForm) {
+  loginForm.addEventListener("submit", handleFormSubmit);
+}
